fc09c8a66cf47093a476af6015c229e1b7a2eafa
Christian Nkobo Michael <chrisnkobo@gmail.com>
2023-10-13 15:56:25 +0100

    flutter_text

--- lib/flutter_text.dart
+++ lib/flutter_text.dart
@@ -31,49 +31,51 @@ class _ChrisHomePageState extends State<ChrisHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Chris Application'),
-        backgroundColor: Colors.green,
-      ),
-      body: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            height: 200,
-            padding: const EdgeInsets.all(8.0),
-            child: const Text(
-              'Welcome to AQskill',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 235, 18, 14),
+        appBar: AppBar(
+          title: const Text('Chris Application'),
+          backgroundColor: Colors.green,
+        ),
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 200,
+              padding: const EdgeInsets.all(8.0),
+              child: const Text(
+                'Welcome to AQskill',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 235, 18, 14),
+                ),
               ),
             ),
-          ),
-          RichText(
-            text: TextSpan(
-                text: "Don't have an account?  ",
-                style: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400),
-                children: [
+            RichText(
+                text: TextSpan(
+                    text: "Don't have account?? ",
+                    style: const TextStyle(
+                      fontSize: 17.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400,
+                      wordSpacing: 1.0,
+                    ),
+                    children: [
                   const WidgetSpan(child: Icon(Icons.supervised_user_circle)),
                   TextSpan(
                       text: ' Sign Up',
                       style: const TextStyle(
-                          color: Color.fromARGB(255, 9, 158, 191),
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16),
+                        fontSize: 17.4,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.w500,
+                        color: Color.fromARGB(255, 8, 168, 217),
+                      ),
                       recognizer: TapGestureRecognizer()
                         ..onTap = () {
-                          print('Hello, thank you for signing up');
+                          print("You have clicked sign up");
                         }),
-                ]),
-          )
-        ],
-      ),
-    );
+                ]))
+          ],
+        ));
   }
 }
